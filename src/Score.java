5c04da1f1f992538bdfda90e01d2fd155e1d9a94
Kevin T. Chen <k_chen31@u.pacific.edu>
2018-03-28 16:30:24 -0700

    getter/setters for Score class

--- src/Score.java
+++ src/Score.java
@@ -1,4 +1,27 @@
 
 public class Score {
-
+	private String name;
+	private int score;
+	
+	public Score() {
+		name = "";
+		score = 0;
+	}
+	public Score(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
 }
