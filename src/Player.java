f4a2fd346be0244f543ddc5191dc060d1351a520
shuyn <s_huynh24@u.pacific.edu>
2018-03-28 17:05:56 -0700

    Outline of of Player file

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,15 @@
 
 public class Player {
-
+	
+// has an object of type fish
+	
+	//moves player fish
+	public void move() {
+		
+	}
+	
+	//checks if player fish has collided with anything
+	public boolean collision() {
+		return false;
+	}
 }
