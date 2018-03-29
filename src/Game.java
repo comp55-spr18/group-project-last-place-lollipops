8ed03ed91b960c8b16cc7811cbeb85cba7ccea37
Kyla Kariger <k_kariger@u.pacific.edu>
2018-03-28 17:02:23 -0700

    just began on layout of the game.

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,30 @@
-
+//kyla & dmitrij
 public class Game {
+	
+public Player person;
+private int score;
+private int level;
+
+//checks if player lost 
+//check size
+// if hit
+// if touch rock die
+boolean checkLose(){
+	if(person.size())
+	return false;
+	
+}
+//when the player wants to pause the game
+void pause() {
+	
+}
+// closes application
+void close() {
+	
+}
+//continues the game after they paused
+void resume() {
+	
+}
 
 }
