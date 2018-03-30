fdf0083e87c76b16cec7199818f53b35232935b7
shuyn <s_huynh24@u.pacific.edu>
2018-03-30 15:18:46 -0700

    Completed collision()

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,6 @@
-
 public class Player {
 	// type Entity allows access to fish
-	private Entity f;
+	private Entity p; //player fish p
 	
 	//moves player fish
 	public void move() {
@@ -9,16 +8,28 @@ public class Player {
 	}
 	
 	//checks if player fish has collided with anything
-	public void collision() {
+	public boolean collision() {
+		double xWidth = p.getxPosition() + p.getWidth() + 1; // x coordinate & width of fish
+		double yHeight = p.getyPosition() + p.getHeight()/2; // y coordinate & height of fish
+
+		if(getElementAt(xWidth, yHeight) != null) { // if fish ran into something, return true
+			return true;
+		}
+		
+		return false; // else return false
+	
+	}
+
+	public void collided() {
+		//call function of whatever [type] it collided with  */
 		boolean collided = false;
 		//check player size and coordinates
 		
 		//did the player collide with anything?
-		if(collided) {
+		if(collision()) {
 			//if so what?
-			
-		}
-		//call function of whatever [type] it collided with
-
+		} 
+		
 	}
+	
 }
