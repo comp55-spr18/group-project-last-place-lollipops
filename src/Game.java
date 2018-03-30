4417d2041921520367b1f3a5004188a05f943176
Kyla Kariger <k_kariger@u.pacific.edu>
2018-03-30 15:14:14 -0700

    workin on close and actionperformed

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,36 @@
 //kyla & dmitrij
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javafx.event.ActionEvent;
+
 public class Game {
 	
 public Player person;
 private int score;
 private int level;
+volatile boolean isPaused = false;
+volatile boolean isClose = false;
+int close;
+private int resume;
+
+public void run() {
+	addMouseListerners();
+	
+}
+
+
+private void addMouseListerners() {
+	// TODO Auto-generated method stub
+	
+}
+
 
 //checks if player lost 
 //check size
 // if hit
 // if touch rock die
+
 //boolean checkLose(){
 //	if(person.size())
 //	return false;
@@ -16,16 +38,25 @@ private int level;
 //}
 //when the player wants to pause the game
 //https://gamedev.stackexchange.com/questions/25177/implementing-pausing-functionality
-void pause() {
+boolean pause() {
+	if(isPaused =true) {
+	System.out.println("Game paused.");
+	}
+	return isPaused;
 	
 }
 // closes application
 void close() {
-	
+	if (isClose=true) {
+		System.exit(0);
+	}
 }
 //continues the game after they paused
 void resume() {
 	
 }
 
+public void actionPerformed(ActionEvent e) {
+	
+}
 }
