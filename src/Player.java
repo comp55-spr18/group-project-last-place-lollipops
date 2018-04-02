478ba50b1e5630d8e708b2d2972d72a11caf27e8
shuyn <s_huynh24@u.pacific.edu>
2018-04-01 22:26:36 -0700

    move function added

--- src/Player.java
+++ src/Player.java
@@ -1,35 +1,42 @@
+
 public class Player {
 	// type Entity allows access to fish
 	private Entity p; //player fish p
-	
 	//moves player fish
-	public void move() {
-		
-	}
+
 	
+	public void move(int xPosition, int yPosition)
+	{
+		p.setxPosition(p.getxPosition()+xPosition);
+		p.setyPosition(p.getyPosition() + yPosition);
+	}
 	//checks if player fish has collided with anything
-	public boolean collision() {
+	public boolean checkCollision() {
 		double xWidth = p.getxPosition() + p.getWidth() + 1; // x coordinate & width of fish
 		double yHeight = p.getyPosition() + p.getHeight()/2; // y coordinate & height of fish
 
-		if(getElementAt(xWidth, yHeight) != null) { // if fish ran into something, return true
+		//if(getElementAt(xWidth, yHeight) != null) { // if fish ran into something, return true
 			return true;
 		}
 		
-		return false; // else return false
-	
-	}
+	//	return false; // else return false
+//	}
 
-	public void collided() {
+	public void collision() {
+		if(checkCollision()) {
+		
 		//call function of whatever [type] it collided with  */
-		boolean collided = false;
 		//check player size and coordinates
 		
 		//did the player collide with anything?
-		if(collision()) {
 			//if so what?
 		} 
 		
+	}
+	
+	public int grow(int s) {
+		
+		return 0;
 	}
 	
 }
