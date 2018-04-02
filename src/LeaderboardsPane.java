4d8a91da2d905ff941e234495b9854acc7bf6251
Dmitrij <d_boyd2@u.pacific.edu>
2018-04-01 18:56:08 -0700

    Added GParagraph for leaderboards

--- src/LeaderboardsPane.java
+++ src/LeaderboardsPane.java
@@ -1,28 +1,43 @@
 import java.awt.event.MouseEvent;
 
+import java.io.*;
+
+
 import acm.graphics.GObject;
 
 public class LeaderboardsPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GButton sound;
+	private Score[] leaderboards;
+	private GParagraph topTen;
 	private GButton back;
-
-	public LeaderboardsPane(MainApplication app) {
+	/*private FileReader in = null;
+	private FileOutputStream out = null;
+	private BufferedReader reader;
+	*/
+	public LeaderboardsPane(MainApplication app) /*throws IOException*/ {
 		this.program = app;
-		sound = new GButton("Sound on/off", 200, 200, 200, 100);
+		topTen = new GParagraph("THIS" + " IS\n" + "WHERE" + " THE\n" + "SCORES" + " GO\n", 200, 200);
 		back = new GButton("Back", 200, 400, 200, 100);
+		leaderboards = new Score[11];
+		/*File f = new File("leaders.txt");
+		if(!f.exists()) {
+			f.createNewFile();
+		}
+		in = new FileReader("leaders.txt");
+		reader = new BufferedReader(in);
+		*/
 	}
 
 	@Override
 	public void showContents() {
-		program.add(sound);
+		program.add(topTen);
 		program.add(back);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(sound);
+		program.remove(topTen);
 		program.remove(back);
 	}
 
@@ -32,9 +47,7 @@ public class LeaderboardsPane extends GraphicsPane {
 		if (obj == back) {
 			program.switchToMenu();
 		}
-		if (obj == sound) {
-			
-		}
+
 	}
 
 }
