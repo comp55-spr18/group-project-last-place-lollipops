04a59017514defd4f84e7a6240c41fe8a2700ed1
shuyn <s_huynh24@u.pacific.edu>
2018-04-03 12:27:59 -0700

    Made sub class for enemy fishes

---
+++ src/EnemyFish.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class EnemyFish extends Fish{
+	private ArrayList<Fish> enemies =  new ArrayList<Fish>();
+
+	//constructor
+	public EnemyFish(double speed, int size, double x, double y, Color color) {
+		setSpeed(speed);
+		setSize(size);
+		setxPosition(x);
+		setyPosition(y);
+		setColor(Color.red);
+	}
+	
+	//functions
+	public void addEnemyFish() {
+		for(Fish f: enemies) {
+			// *** add fishes ***
+		}
+	}
+	
+	//public void moveEnemy() ???
+}
