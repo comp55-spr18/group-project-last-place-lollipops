25690ce6ab9f621c44e3ab1fc16e8615a5427a81
shuyn <s_huynh24@u.pacific.edu>
2018-04-03 12:28:44 -0700

    Made Class for fishes

---
+++ src/Fish.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.util.ArrayList;
+import acm.graphics.GImage;
+
+public class Fish extends Entity{ // EnemyFish & Player inherit from this class
+	private int size;
+	
+	//getters & setters
+	public int getSize() { 
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	// functions that both player fish & enemyFish would use
+
+}
