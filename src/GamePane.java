9512226606dcd331abef91a8b46bb808a378806c
Kyla <k_kariger@u.pacific.edu>
2018-04-03 21:40:41 -0700

    added image,& title of game at top

--- src/GamePane.java
+++ src/GamePane.java
@@ -1,40 +1,53 @@
 //kyla
 import java.awt.event.MouseEvent;
-import acm.graphics.GObject;
-
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import acm.graphics.*;
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+
 public class GamePane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 	// all of the GraphicsProgram calls
 	private GButton pause;
 	
-	private GRect p1;
-	private Player p;
+	//private GRect p1;
+	//private Player p;
+	
+	private GImage img;
+	private GParagraph para;
 	
 	public GamePane(MainApplication app) {
 		this.program = app;
+		
+		img = new GImage("GamePane.jpg", 100, 100);
+		para = new GParagraph("Something Smells Fishy", 50, 30);
+		para.setFont("Arial-30");
+		para.setColor(Color.red);
 		pause = new GButton("||", program.WINDOW_WIDTH, 10, 50, 50);
 		pause.setLocation(pause.getX() - pause.getWidth() - 10, pause.getY());
-		
-		p = new Player();
-		p1 = new GRect(program.WINDOW_WIDTH/2, program.WINDOW_HEIGHT/2, p.p.getSize()*5,p.p.getSize()*5);
-		p1.setFillColor((p.p.getColor()));
-		p1.setFilled(true);
+		pause.setFillColor(Color.RED);
+		//p = new Player();
+		//p1 = new GRect(program.WINDOW_WIDTH/2, program.WINDOW_HEIGHT/2, p.p.getSize()*5,p.p.getSize()*5);
+		//p1.setFillColor((p.p.getColor()));
+		//p1.setFilled(true);
 		
 	}
 
 	@Override
 	public void showContents() {
 		program.add(pause);
-		program.add(p1);
+		//program.add(p1);
+		program.add(img);
+		program.add(para);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(pause);
-		program.remove(p1);
+		//program.remove(p1);
+		program.remove(img);
+		program.remove(para);
 	}
 
 	@Override
@@ -45,6 +58,7 @@ public class GamePane extends GraphicsPane {
 		}
 
 	}
+	/*
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
@@ -92,5 +106,5 @@ public class GamePane extends GraphicsPane {
 		}
 	}
 
-
+*/
 }
