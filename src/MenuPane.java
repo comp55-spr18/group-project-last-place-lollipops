2940ef9be6aef8ff71328343501f0f20778ff5f0
Kyla Kariger <k_kariger@u.pacific.edu>
2018-04-06 15:07:00 -0700

    added background and centered images

--- src/MenuPane.java
+++ src/MenuPane.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
 public class MenuPane extends GraphicsPane {
@@ -11,38 +13,52 @@ public class MenuPane extends GraphicsPane {
 	private GButton instructions;
 	private GButton leaderboards;
 	private GButton quit;
+	private GImage background;
+	private GLabel title;
 
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		play = new GButton("Play", 200, 100, 200, 50);
-		play.setFillColor(Color.RED);
-		settings = new GButton("Settings", 200, 200, 200, 50);
-		settings.setFillColor(Color.RED);
-		instructions = new GButton("Instructions", 200, 300, 200, 50);
-		instructions.setFillColor(Color.RED);
-		leaderboards = new GButton("Leaderboards", 200, 400, 200, 50);
-		leaderboards.setFillColor(Color.RED);
-		quit = new GButton("Quit",200, 500, 200, 50);
-		quit.setFillColor(Color.red);
+		title = new GLabel ("Something Smells Fishy", 50, 50);
+		title.setFont("Forte-70");
+		background = new GImage("flower.gif",0,0);
+		background.setBounds(0, 0, program.WINDOW_WIDTH, program.WINDOW_HEIGHT);
+		background.sendBackward();
+		play = new GButton("Play", 300, 100, 200, 50);
+		play.setFillColor(Color.white);
+		settings = new GButton("Settings", 300, 200, 200, 50);
+		settings.setFillColor(Color.white);
+		instructions = new GButton("Instructions", 300, 300, 200, 50);
+		instructions.setFillColor(Color.white);
+		leaderboards = new GButton("Leaderboards", 300, 400, 200, 50);
+		leaderboards.setFillColor(Color.white);
+		quit = new GButton("Quit",300, 500, 200, 50);
+		quit.setFillColor(Color.white);
+		
+	
 	}
 
 	@Override
 	public void showContents() {
+		program.add(background);
+		program.add(title);
 		program.add(play);
 		program.add(settings);
 		program.add(instructions);
 		program.add(leaderboards);
 		program.add(quit);
 	}
+	
 
 	@Override
 	public void hideContents() {
+		program.remove(title);
 		program.remove(play);
 		program.remove(settings);
 		program.remove(instructions);
 		program.remove(leaderboards);
 		program.remove(quit);
+		program.remove(background);
 	}
 
 	@Override
