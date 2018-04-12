a202e502c69e18f1cb7606c1e17b9968ef923b56
shuyn <s_huynh24@u.pacific.edu>
2018-04-12 13:21:56 -0700

    Created Wave class

---
+++ src/Wave.java
@@ -0,0 +1,34 @@
+
+public class Wave {
+	
+	//wave is over when score = something
+	// ressets variables enemy size
+	//resets counter for when enemies spawn
+	//enemy to spawn
+	// wave 0
+		//officially spawn wave 1 to triger waves
+		//or delay it
+	
+	// get next enemy (this is what counter is for - randomize it)
+		//switch
+			// case new enemy (game, #) game allows access to where the player is (honing device)
+	//if garbage gets triggered (random num genereator)
+	// trigger garbage
+	
+	//update for new waves
+	
+	//check if lose
+	
+	//if the maxSize of array is less than max enemies
+		//then add a fish
+		//do you want it to spawn on the left or the right? then do rgen
+		//new Fish(game, x, rgen())
+		//when fish gets eaten remove from array
+	
+	//destroy fish when the sprite bounds are out of bounds
+		//if x or y < 0 (max width)
+		//get rid of the fish from the array and everything about it
+		// new fish will be spawn because maxSize < max enemies
+	
+	//** need delay so all fish dont spawn at the same time*** until you hit the max size
+	}
