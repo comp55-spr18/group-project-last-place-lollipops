c7ef51171fc3cbf9a74439d45c29f2bfffb10df0
Kyla Kariger <k_kariger@u.pacific.edu>
2018-04-16 21:30:32 -0700

    Graphics done for lose pane

--- src/LosePane.java
+++ src/LosePane.java
@@ -1,8 +1,8 @@
 //kyla 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.TextField;
 import java.awt.event.MouseEvent;
-import javax.swing.JTextField;
-
 import acm.graphics.*;
 
 
@@ -14,28 +14,53 @@ public class LosePane extends GraphicsPane {
 	private GButton back;
 	private GLabel loseLabel;
 	private GImage background;
-	private GRectangle rect1;
-	
+	private GButton saveName;
+	private GLabel playername;
+	private TextField nameEnter;
+	private GRect rect1;
+	private GLabel yourscore;
 	
 	public LosePane(MainApplication app) {
 		this.program = app;
-		
-
-		Score name = getName();
-		
-		
 		loseLabel = new GLabel( "You Died!", 220 , 100);
 		loseLabel.setFont("Century Gothic-bold-70");
 		loseLabel.setColor(Color.blue);
-		sound = new GButton ("Sound on",60,300,200,50 );
+		
+		Score score = getScore();
+		
+		yourscore= new GLabel("Your Score: "+score, 200,170);
+		yourscore.setFont("Century Gothic-30");
+		
+		playername=new GLabel("Please enter your name:", 200,250);
+		playername.setFont("Century Gothic-30");
+		
+		nameEnter = new TextField();
+		nameEnter.setBounds(280,300,200,50);
+		Font font1 = new Font("Century Gothic", Font.BOLD, 50);
+		nameEnter.setFont(font1);
+		saveName = new GButton("Save",280,400,200,50);
+		
+		Score name = getName();
+		
+		rect1=new GRect(180,45,400,230);
+		rect1.setFillColor(Color.white);
+		rect1.setFilled(true);
+		rect1.setColor(Color.black);
+		
+		
+		sound = new GButton ("Sound on",60,500,200,50 );
 		sound.setFillColor(Color.RED);
-		back = new GButton("Back to Menu",280,300,200,50);
+		back = new GButton("Back to Menu",280,500,200,50);
 		back.setFillColor(Color.RED);
-		quit = new GButton("Quit Game", 500,300,200,50);
+		quit = new GButton("Quit Game", 500,500,200,50);
 		quit.setFillColor(Color.RED);
 		background = new GImage("fishbackground.gif",0,0);
 		background.setBounds(0, 0, program.WINDOW_WIDTH, program.WINDOW_HEIGHT);
-		rect1 = new GRectangle( 200,100,200,50);
+	}
+
+	private Score getScore() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	private Score getName() {
@@ -46,7 +71,12 @@ public class LosePane extends GraphicsPane {
 	@Override
 	public void showContents() {
 		program.add(background);
+		program.add (rect1);
 		program.add(loseLabel);
+		program.add(yourscore);
+		program.add(nameEnter);
+		program.add(saveName);
+		program.add(playername);
 		program.add(sound);
 		program.add(quit);
 		program.add(back);
@@ -55,8 +85,12 @@ public class LosePane extends GraphicsPane {
 	@Override
 	public void hideContents() {
 		program.remove(background);
-	
+		program.remove(nameEnter);
 		program.remove(loseLabel);
+		program.remove(playername);
+		program.remove(saveName);
+		program.remove(rect1);
+		program.remove(playername);
 		program.remove(sound);
 		program.remove(quit);
 		program.remove(back);
@@ -87,6 +121,9 @@ public class LosePane extends GraphicsPane {
 		
 		if(obj == quit) {
 			System.exit(0);
+		}
+		if(obj ==saveName) {
+			
 		}
 		
 	}
